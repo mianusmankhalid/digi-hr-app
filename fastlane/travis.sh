2279c44a77d80c90c1b249e52e05042c078e93a9
JIWA350164 <amir.ali.jiwani@exact.com>
2018-01-25 14:29:24 +0800

    Adds goals to the travis build mechanism

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -5,14 +5,18 @@
 #   exit $?
 # fi
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-#  git fetch --unshallow
-  fastlane android dev
-  exit $?
-fi
+# if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+# #  git fetch --unshallow
+#   fastlane android dev
+#   exit $?
+# fi
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false"]]; then
   git fetch --unshallow
   fastlane android push distribution:"RC"
+  exit $?
+else
+  # Run for any other commit
+  fastlane android dev
   exit $?
 fi
