212e1af99903abff6a933bd2083b8d715420cba3
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2018-03-01 22:38:27 +0800

    Uses Multi Regular font as supplied by the organization

--- app/pages/landing/splash.js
+++ app/pages/landing/splash.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import theme from "@digihr_app_config/theme";
-import images from "@digihr_assets/images";
+import theme from '@digihr_app_config/theme';
+import images from '@digihr_assets/images';
 
 export default class Splash extends Component {
-  
-    render() {
-      return (
-        <View style={styles.container}>
-            <View style={styles.titleWrapper}>
-                <Image style={styles.image} source={images.altLogo} />
-            </View>
-            <View>
-                <Text style={styles.footer}>alt.hr</Text>
-            </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titleWrapper}>
+          <Image style={styles.image} source={images.altLogo} />
         </View>
-      );
-    }
+        <View>
+          <Text style={styles.footer}>alt.hr</Text>
+        </View>
+      </View>
+    );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-        backgroundColor: theme.background.colors.white,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    footer: {
-        fontFamily: 'Muli-Regular',
-        color: theme.colors.grey,
-        fontWeight: 'bold',
-        paddingBottom: 20,
-    },
-    titleWrapper: {
-        justifyContent: 'center',
-        flex: 1
-    },
-    image: {
-        width: 200,
-        height: 200
-    }
-  });
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.background.colors.white,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    fontFamily: 'Muli Regular',
+    color: theme.colors.grey,
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 20,
+  },
+  titleWrapper: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
