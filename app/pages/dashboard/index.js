673f863b631195d5d57dbf5574a9509658350769
Usman Khalid <mianusman@msn.com>
2018-03-22 14:23:10 +0800

    Bumps the version 0.0.8 --upload-to-appetize

--- app/pages/dashboard/index.js
+++ app/pages/dashboard/index.js
@@ -6,7 +6,7 @@ export default class DashboardScreen extends Component {
   constructor(props) {
     super(props);
     this.props.nav_helper.setScreenParams({
-      headerBackTitle: 'BackToPage1',
+      headerBackTitle: 'Dashboard',
     });
   }
 
